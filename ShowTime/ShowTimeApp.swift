a546037e2c384af033b016a6aaa95477a2461477
Niko <niko.holm@edu.sakky.fi>
2026-06-16 15:56:01 +0300

    Initial Commit

---
+++ ShowTime/ShowTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowTimeApp.swift
+//  ShowTime
+//
+//  Created by Niko Holm on 16.6.2026.
+//
+
+import SwiftUI
+
+@main
+struct ShowTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShowTimeTests/ShowTimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShowTimeTests.swift
+//  ShowTimeTests
+//
+//  Created by Niko Holm on 16.6.2026.
+//
+
+import Testing
+@testable import ShowTime
+
+struct ShowTimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
